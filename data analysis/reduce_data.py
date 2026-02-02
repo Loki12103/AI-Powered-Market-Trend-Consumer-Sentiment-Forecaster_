71f687bdb2d3ccecab3e282e75c93091c1195416
Lokesh K <lokeshsh5252@gmail.com>
2026-02-02 21:26:52 +0530

    Update project files and add Agile template

--- data analysis/reduce_data.py
+++ data analysis/reduce_data.py
@@ -1,18 +1,3 @@
-import pandas as pd
-
-df = pd.read_csv("combined_cleaned_data.csv")
-
-# kee only 30% of the data
-KEEP_RATIO = 0.3
-
-
-df_reduced =(
-    df.groupby(['product', 'rating'], group_keys=False)   #grouiping product and rating
-    .apply(lambda x:x.sample(n=max(1, int(len(x)*KEEP_RATIO)), random_state=42))
-    .reset_index(drop=True)    #resetting index
-)
-
-print(f"Original data size: {len(df)}")
-print(f"Reduced data size: {len(df_reduced)}")
-
-df_reduced.to_csv("reduced_combined_cleaned_data.csv", index=False)
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac04d02322cccd3698b6e5a80c3120abbb96c8a430cc4e6cd6865948d31d9beb
+size 520
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# from notification.notification import *
-
-# # send_mail(text="Hello this is test user", subject="Testing mail")
-
-# send_slack_notification(text="Hello this is my second notification")
+version https://git-lfs.github.com/spec/v1
+oid sha256:a5428b433b0592e9d394372211fa8e0483ae733c20d2381bababa5108ad2db44
+size 190
